2ab8f331acdd47f596a88511ff7d81a06b627b11
1025900509 <74659156+1025900509@users.noreply.github.com>
2020-11-23 11:53:33 +0800

    Update 112301.App.java

--- 112301.App.java
+++ 112301.App.java
@@ -5,12 +5,17 @@ import java.util.Scanner;
 public class AOP {
     public static void main(String[] args) {
         Scanner scann = new Scanner(System.in);
-            System.out.println("请输入一个数字");
+            System.out.println("请输入数字1");
             while (true) {
             int a = scann.nextInt();
-            int z = (int) (Math.random() * a*100%10);
-            int f = (int) (Math.random() * a*100%10);
-            System.out.println("生成的随机坐标为"+z+","+f);
+            int z = (int) (Math.random() * a*100%100);
+            int f = (int) (Math.random() * a*100%100);
+            if(a==1){
+              System.out.println("生成的随机坐标为"+z+","+f);
+            }else{
+                System.out.println("请输入数字1");
+                }
+
         }
     }
 }
